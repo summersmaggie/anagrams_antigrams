ee3e70b6466534970c88cd6373f61756cd2f1a27
Maggie Summers <summers.maggie@gmail.com>
2018-02-09 09:32:17 -0800

    add third meaningful test spec to write a rule that requires input to contain a vowel

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -11,8 +11,8 @@ describe ("anagrams_antigrams") do
     test2 = Input.new("Tea", "Eat")
     expect(test2.anagram()).to(eq("These words are anagrams!"))
   end
-  # it("checks that the input has vowels") do
-  #   test2 = Input.new("yurb", "ruby")
-  #   expect(test2.anagram()).to(eq("You need to input actual words!"))
-  # end
+  it("checks that the input has vowels") do
+    test2 = Input.new("yurb", "ruby")
+    expect(test2.anagram()).to(eq("You need to input actual words!"))
+  end
 end
